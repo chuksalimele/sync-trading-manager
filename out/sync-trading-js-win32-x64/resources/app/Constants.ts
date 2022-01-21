8fe2cf7b8964ae30fbb6a9e116d1bb1c6fb56355
Chuks Alimele <chuksalimele@gmail.com>
2022-01-21 22:53:20 +0100

    Added duplicate EA validation

--- out/sync-trading-js-win32-x64/resources/app/Constants.ts
+++ out/sync-trading-js-win32-x64/resources/app/Constants.ts
@@ -9,10 +9,6 @@ export class Constants {
     public static readonly MAX_MODIFY_RETRY: number = 3;
     public static readonly MAX_PLACE_ORDER_RETRY: number = 3; 
     
-    public static readonly trade_condition_not_changed: string = "no error, trade conditions not changed";
-
-    public static readonly no_changes: string = "no changes";
-    
     public static readonly APPROX_ZERO_TOLERANCE: number = 0.000000001;
 
     public static readonly IN_PROGRESS: number = 0;
@@ -27,4 +23,14 @@ export class Constants {
     public static readonly Instant_when_both_accounts_have_credit_bonuses: string = 'Instant when both accounts have credit bonuses';
     public static readonly Pending_at_price: string = 'Pending at price';
     public static readonly Pending_at_price_when_both_accounts_have_credit_bonuses: string = 'Pending at price when both accounts have credit bonuses';
+
+    //errors
+    public static readonly ERR_DUPLICATE_EA: string = "DUPLICATE EA";
+    public static readonly ERR_TRADE_CONDITION_NOT_CHANGED: string = "no error, trade conditions not changed";
+    public static readonly ERR_NO_CHANGES: string = "no changes";
+    
+    //commands
+    public static readonly CMD_DUPLICATE_EA: string = "duplicate_ea";
+    public static readonly CMD_CHECK_ENOUGH_MONEY: string = "check_enough_money";
+
 }
